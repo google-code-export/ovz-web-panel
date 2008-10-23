6760fecbe5b1234df6962af2a70d1e20a93d1be0
sp <sp@5ed322ce-bccc-11de-a0e9-83d03c287164>
2008-10-23 16:11:22 +0000

    move models to root directory cause separation by modules is redundant now

--- ovz-web-panel/init.php
+++ ovz-web-panel/init.php
@@ -4,7 +4,7 @@ define('ROOT_PATH', dirname(__FILE__));
 
 set_include_path(get_include_path() 
 	. PATH_SEPARATOR . ROOT_PATH . '/externals/'
-	. PATH_SEPARATOR . ROOT_PATH . '/modules/default/models/'
+	. PATH_SEPARATOR . ROOT_PATH . '/models/'
 );
 
 require_once('Zend/Loader.php');
