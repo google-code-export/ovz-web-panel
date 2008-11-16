76fd5beb37e09059896808d5f289e8fed0559577
sp <sp@5ed322ce-bccc-11de-a0e9-83d03c287164>
2008-11-16 08:03:52 +0000

    fix misprint in surname

--- ovz-web-panel/trunk/htdocs/js/layouts/abstract.js
+++ ovz-web-panel/trunk/htdocs/js/layouts/abstract.js
@@ -6,7 +6,7 @@ Owp.Layouts.Abstract.onCopyrightClick = function() {
 		icon: Ext.MessageBox.INFO,
 		buttons: Ext.Msg.OK,
 		animEl: 'layoutCopyrightInner',
-		msg: 'Author: <a href="mailto:alex@softunity.com.ru">Alexei Yuhzakov</a><br/>' +
+		msg: 'Author: <a href="mailto:alex@softunity.com.ru">Alexei Yuzhakov</a><br/>' +
 			'Web site: <a href="http://code.google.com/p/ovz-web-panel/" target="_blank">' +
 				'http://code.google.com/p/ovz-web-panel/</a><br/><br/>' +
 			'&copy; Copyright 2008 SoftUnity.<br/>All Rights Reserved.'
