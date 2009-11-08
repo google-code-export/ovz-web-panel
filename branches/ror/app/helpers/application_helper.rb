d51bed13fe54ef0c35e0938e5d67a73cde576f0e
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2009-11-08 08:17:55 +0000

    Overriding default translate helper to allow fallback.

--- branches/ror/app/helpers/application_helper.rb
+++ branches/ror/app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def translate(key, options = {})
+    I18n.translate(key, options)
+  end
+  alias :t :translate
+  
 end
