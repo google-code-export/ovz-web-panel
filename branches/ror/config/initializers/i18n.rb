850d3ab89a2bc3395f2e5e4c0398bafb4cd13878
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2009-11-08 06:47:46 +0000

    Adding fallback to English for missed locale keys.

--- branches/ror/config/initializers/i18n.rb
+++ branches/ror/config/initializers/i18n.rb
@@ -9,10 +9,23 @@ module I18n
     end
   end
 
-  class << self
+  class << self    
     def available_locales
       backend.available_locales
+    end
+    
+    def fallback(exception, locale, key, options)
+      if (MissingTranslationData === exception) && (locale != self.default_locale)
+        begin
+          return translate(key, options.merge(:locale => self.default_locale, :raise => true))
+        rescue MissingTranslationData
+        end
+      end
+      
+      send :default_exception_handler, exception, locale, key, options
     end
   end
   
 end
+
+I18n.exception_handler = :fallback
