251c35071fe31539a25a1e658db74044ce8afead
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2009-11-09 15:01:47 +0000

    Saving state for admin's west panel and "My Profile" window.

--- branches/ror/public/javascripts/application.js
+++ branches/ror/public/javascripts/application.js
@@ -1,2 +1,7 @@
-Ext.BLANK_IMAGE_URL = '/images/blank.gif';
+(function() {
+  
+  Ext.BLANK_IMAGE_URL = '/images/blank.gif';
 
+  Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+  
+})();
