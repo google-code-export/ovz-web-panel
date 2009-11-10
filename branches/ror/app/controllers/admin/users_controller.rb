9d59fb544e6940096ee0acf6fe0e407973152186
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2009-11-10 16:30:00 +0000

    Improving forms validation visualization.

--- branches/ror/app/controllers/admin/users_controller.rb
+++ branches/ror/app/controllers/admin/users_controller.rb
@@ -1,19 +1,14 @@
 class Admin::UsersController < AdminController
   
-  def save_profile
-    if params[:current_password].blank? && params[:new_password].blank?
-      render :json => { :success => true }  
-      return
-    end
-    
+  def save_profile    
     user = User.authenticate(@current_user.login, params[:current_password])
     
     if !user
-      render :json => { :success => false, :message => t('admin.my_profile.bad_current_password') }
+      render :json => { :success => false, :errors => [['current_password', t('admin.my_profile.bad_current_password')]] }
       return
     end
         
-    user.password = params[:new_password]
+    user.password = params[:password]
     user.password_confirmation = params[:confirm_password]
     
     if user.save
--- branches/ror/public/javascripts/application.js
+++ branches/ror/public/javascripts/application.js
@@ -1,7 +1,55 @@
-(function() {
-  
+(function() {  
   Ext.BLANK_IMAGE_URL = '/images/blank.gif';
 
   Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
   
+  Ext.QuickTips.init();
+  
+  // turn on validation errors beside the field globally
+  Ext.form.Field.prototype.msgTarget = 'side';
 })();
+
+Ext.ns('Owp.form');
+
+Owp.form.errorHandler = function(form, action, params) {
+  if ('client' == action.failureType) {
+    return
+  }
+  
+  // show overall status message
+  if ('undefined' != typeof action.result.message) {
+    Ext.MessageBox.show({
+      msg: action.result.message,
+      buttons: Ext.MessageBox.OK,
+      icon: Ext.MessageBox.ERROR,
+      fn: params['fn']
+    });
+    
+    return
+  }
+  
+  // highlight fields with errors
+  var errorsHash = new Array();  
+  
+  Ext.each(action.result.errors, function(message) {
+    messageField = message[0];
+    messageContent = message[1];
+    
+    errorsHash[messageField] = (errorsHash[messageField])
+      ? errorsHash[messageField] + '<br/>' + messageContent
+      : messageContent;
+  });
+    
+  for (var index in form.items.items) {
+    if ('function' == typeof errorsHash[index]) {
+      continue
+    }
+ 
+    var field = form.items.items[index];
+    
+    if (('undefined' != field.name) && ('undefined' != typeof errorsHash[field.name])) {
+        field.markInvalid(errorsHash[field.name])
+    }
+  }
+    
+}
