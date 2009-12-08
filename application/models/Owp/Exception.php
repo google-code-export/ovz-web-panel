0dd4cc920a70b8a0e7b5bfe8306f6cf596d08dfb
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2009-12-08 16:27:02 +0000

    Move current trunk to branch for freezing.

---
+++ application/models/Owp/Exception.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Generic exception
+ *
+ * @author Alexei Yuzhakov <alex@softunity.com.ru>
+ */
+class Owp_Exception extends Zend_Exception
+{
+
+}
---
+++ application/modules/default/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Index controller
+ *
+ * @author Alexei Yuzhakov <alex@softunity.com.ru>
+ */
+class IndexController extends Owp_Controller_Action_Simple
+{
+
+    public function indexAction()
+    {
+        $this->_redirect('/login');
+    }
+
+}
