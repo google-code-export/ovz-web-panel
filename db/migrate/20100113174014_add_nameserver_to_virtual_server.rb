f2f6764e94666c59792e7cf66603b436a804a12c
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2010-01-19 15:55:32 +0000

    Tag for 0.5 preview release.

--- db/migrate/20100113174014_add_nameserver_to_virtual_server.rb
+++
@@ -1,9 +0,0 @@
-class AddNameserverToVirtualServer < ActiveRecord::Migration
-  def self.up
-    add_column :virtual_servers, :nameserver, :string
-  end
-
-  def self.down
-    remove_column :virtual_servers, :nameserver
-  end
-end
--- db/migrate/20100114154437_add_diskspace_to_virtual_server.rb
+++
@@ -1,9 +0,0 @@
-class AddDiskspaceToVirtualServer < ActiveRecord::Migration
-  def self.up
-    add_column :virtual_servers, :diskspace, :integer, :default => 1024
-  end
-
-  def self.down
-    remove_column :virtual_servers, :diskspace
-  end
-end
