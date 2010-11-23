39ff45a81802d41b4d115eada433872a8cf1ab1a
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2010-11-23 15:27:25 +0000

    Make v1.7 as default candidate for installation.

--- installer/ai.sh
+++ installer/ai.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # global variables
-VERSION="1.6"
+VERSION="1.7"
 DOWNLOAD_URL="http://ovz-web-panel.googlecode.com/files/ovz-web-panel-$VERSION.tgz"
 RUBYGEMS_URL="http://production.cf.rubygems.org/rubygems/rubygems-1.3.5.tgz"
 RUBY_SQLITE3_CMD="ruby -e \"require 'rubygems'\" -e \"require 'sqlite3/database'\""
