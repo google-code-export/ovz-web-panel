d48eba096a20f2f7983145d40b45ab7198613584
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2013-02-23 05:39:34 +0000

    Reset remember_me tokens on upgrade.

--- installer/ai.sh
+++ installer/ai.sh
@@ -213,6 +213,9 @@ install_product() {
 
     puts "Syncing physical servers state..."
     ruby $INSTALL_DIR/script/runner -e production "HardwareServer.all.each { |server| server.sync }"
+
+    puts "Reset remember_me tokens..."
+    ruby $INSTALL_DIR/script/runner -e production "User.all.each{ |user| user.remember_token = ''; user.save }"
   fi
   
   [ ! -x $INSTALL_DIR/script/owp ] && chmod +x $INSTALL_DIR/script/owp
